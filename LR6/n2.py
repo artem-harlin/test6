f265e9d3ca6f2d078c7b45a1ed5e9e2cf5238029
artem-harlin <114216269+artem-harlin@users.noreply.github.com>
2022-10-29 23:19:31 +0300

    Add files via upload

---
+++ LR6/n2.py
@@ -0,0 +1,4 @@
+x = set(map(int,input().split()))
+y = set(map(int,input().split()))
+n = y & x
+print(len(n))
---
+++ LR6/n3.py
@@ -0,0 +1,3 @@
+text = 'I have many favourite artists. Among them are writers, painters and musicians. When I have free time I often read my favourite books or listen to my favourite music.'
+result = len(text.split())
+print("There are " + str(result) + " words.")
